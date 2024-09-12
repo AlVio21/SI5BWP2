752524c3b1a00ce6391c68d99987fec09e22be32
AlVio21 <160828042+AlVio21@users.noreply.github.com>
2024-09-12 12:59:23 +0700

    Pertemuan 2 FINISH

--- Pertemuan2/src/index.js
+++ Pertemuan2/src/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+    res.send('Halaman Tentang Perusahaan');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Halaman Contact');
+});
+
+//middleware
+app.use('*', (req,res) => {
+    res.status(404);
+    res.send("404, Halaman Tidak Ditemukan");
+});
+
 app.listen(port, () => {
     console.log('Server started on port 3000');
 });

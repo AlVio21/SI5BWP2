5e59aebff382d60d67c0f903725c9b2f73eae546
AlVio21 <160828042+AlVio21@users.noreply.github.com>
2024-11-21 12:38:00 +0700

    Tidak Berhasil

--- prjbuku/src/app/services/buku.service.ts
+++ prjbuku/src/app/services/buku.service.ts
@@ -6,9 +6,9 @@ import { Buku } from '../models/buku.model';
   providedIn: 'root'
 })
 export class BukuService {
-  private url : string = "http://localhost:3000/bukus";
+  private url : string = "http://localhost:3000/buku";
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   addBuku(judul : string, penulis : string, genres : string[]){
     const buku : Buku = {
@@ -18,8 +18,8 @@ export class BukuService {
       genre : genres
     }
 
-    // this.http.post<{message : string}>(this.url, buku).subscribe((response)=>{
-    //   console.log(response.message)
-    // });
+    this.http.post<{message : string}>(this.url, buku).subscribe((response)=>{
+      console.log(response.message)
+    });
   }
 }
